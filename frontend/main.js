1e025130948cc1814db6a74c396f73ff6b8d7e6c
mehraDotDev <mehraDotDev@gmail.com>
2025-02-04 11:31:39 +0530

    update backend_endpoint to prod url

--- frontend/main.js
+++ frontend/main.js
@@ -1,2 +1,2 @@
-export const BACKEND_ENDPOINT = 'http://localhost:3000'; // Default endpoint for development
-// export const BACKEND_ENDPOINT = 'https://mlb-montage-maker.uc.r.appspot.com'; // Default endpoint for development
+// export const BACKEND_ENDPOINT = 'http://localhost:3000'; // Default endpoint for development
+export const BACKEND_ENDPOINT = 'https://mlb-montage-maker-backend.mehra.dev';
